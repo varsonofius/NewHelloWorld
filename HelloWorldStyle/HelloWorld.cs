3195aac0774002de3d7dade42b2b074e294c5ce1
varsonofius <129762646+varsonofius@users.noreply.github.com>
2023-12-19 15:43:27 +0300

    Add files via upload

---
+++ HelloWorldStyle/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿namespace HelloWorldStyle
+{
+    public static
+
+        class HelloWorld
+    {
+        public static string ReturnGreeting(string hello, string addressee)
+        {
+            return hello + ", " + addressee + "!";
+        }
+
+        public static int ReturnSum(int a, int b)
+        {
+            return
+                a
+                + b;
+        }
+
+        public static int[] ReturnArray(int length)
+        {
+            var array = new
+                int[
+                    length];
+
+            for
+            (
+                int i = 0; i < length; i++)
+            {
+                array[i] = i;
+            }
+
+            return array;
+        }
+    }
+}
